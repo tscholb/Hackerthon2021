f749e228ba8ac24ad741fff010d593f13cd0092f
Sunghyun Kim <scholb.kim@samsung.com>
2021-07-28 19:02:39 +0900

    [tutorial] add image tutorial

---
+++ Tutorial/BaseComponents/NUIImageView/NUIXAMLProject1/Scene1Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace NUIXAMLProject1
+{
+    public partial class Scene1Page : View
+    {
+        public Scene1Page()
+        {
+            InitializeComponent();
+            //lottieView.Play();
+        }
+
+        private void imgView_ResourceReady(object sender, ImageView.ResourceReadyEventArgs e)
+        {
+            //lottieView.Play();
+            //imgView.AlphaMaskURL = "*Resource*/images/mask.png";
+        }
+    }
+}
